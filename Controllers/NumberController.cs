134a7906affb07936b66452a3bdc31c443862a9f
Karthik Ravutla <Karthik.ravutla@gmail.com>
2025-05-26 19:43:35 -0400

    inital Commit

---
+++ Controllers/NumberController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SampleTest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NumberController : ControllerBase
+    {
+
+        [HttpPost("SumEven")]
+        public IActionResult SumEvenNumbers([FromBody] List<int> numbers)
+        {
+
+            if (numbers == null || numbers.Count == 0) return BadRequest("Provide list of numbers");
+
+            int sum = numbers.Where(x => x % 2 == 0).Sum();
+
+            return Ok(new { Sum = sum });
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,42 @@
+
+namespace SampleTest
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAngularApp", builder => { builder.WithOrigins("http://localhost:4200").AllowAnyHeader().AllowAnyMethod(); });
+            });
+
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+            app.UseCors("AllowAngularApp");
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseRouting();
+            app.UseEndpoints(static endpoints => { endpoints.MapControllers(); });
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
